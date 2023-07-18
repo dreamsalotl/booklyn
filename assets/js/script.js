726c206b9b6c0c88d867e389783f211e9629ee76
angiecruz02 <angelicacruz02@hotmail.com>
2023-07-17 21:53:16 -0500

    create function exploreMovie to display movie cover and movie details

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,10 @@ function displaySearchResults(event) {
         var author = data.works[i].authors[0].name;
         var year = data.works[i].first_publish_year;
         
-        bookResults.appendChild(document.createElement("div"));
-        bookResults.children[i].append(data.works[i][bookTitle]);
-        bookResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
-
-        bookElement.setAttribute("onclick", `exploreItem("${author}", "${year}", "${bookTitle}", "${bookCoverURL}")`);
+      
+        bookElement.setAttribute("onclick", `exploreBook("${author}", "${year}", "${bookTitle}", "${bookCoverURL}")`);
         bookResults.appendChild(bookElement);
+        bookResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
         bookResults.children[i]?.append(bookTitle);
       }
     });
@@ -50,13 +48,15 @@ function displaySearchResults(event) {
         var movieResults = document.getElementById("movieResults");
         var movieElement = document.createElement("div");
         
-         movieResults.appendChild(document.createElement("div"));
-         movieResults.children[i].append(data.description[i][movieTitle]);
-         movieResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
+        var movieActors = data.description[i]?.["#ACTORS"];
+        var movieYear = data.description[i]?.["#YEAR"];
+        var movieCover = data.description[i]?.["#IMG_POSTER"];
 
+        
         console.log("movieTitle", movieTitle);
-        movieElement.setAttribute("onclick", `exploreItem("${movieTitle}")`);
+        movieElement.setAttribute("onclick", `exploreMovie("${movieActors}", "${movieYear}", "${movieTitle}", "${movieCover}")`);
         movieResults.appendChild(movieElement);
+        movieResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
         movieResults.children[i]?.append(movieTitle);
       }
     });
@@ -77,7 +77,7 @@ function clearResults() {
   }
 }
 // Book section 
-function exploreItem(author, year, title, coverURL) {
+function exploreBook(author, year, title, coverURL) {
     var contentElement = document.getElementById("preview-content");
     contentElement.innerHTML = "";
   console.log(
@@ -109,8 +109,26 @@ contentElement.appendChild(authorElement)
   contentElement.appendChild(yearElement)
 }
 
-function exploreMovie() {
-    
-}
+function exploreMovie(movieActors, movieYear, movieTitle, movieCover) {
+    var contentElement = document.getElementById("preview-content");
+    contentElement.innerHTML = "";
+   console.log("Actors", movieActors, "year", movieYear, "title", movieTitle, "cover", movieCover)
+
+    var coverElement = document.createElement("img")
+    coverElement.setAttribute("src", movieCover)
+    contentElement.appendChild(coverElement);
+
+    var titleElement = document.createElement("h3")
+    titleElement.innerText = `Title: ${movieTitle}`
+    contentElement.appendChild(titleElement)
+
+    var actorElement = document.createElement("p")
+    actorElement.innerText = `Actors: ${movieActors}`
+    contentElement.appendChild(actorElement)
+
+    var yearElement = document.createElement("p")
+    yearElement.innerText = `Year: ${movieYear}`
+    contentElement.appendChild(yearElement)
+};
 
 $("#searchBtn").on("click", displaySearchResults);
