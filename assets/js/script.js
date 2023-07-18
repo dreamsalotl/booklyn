6ead75c48ddcde9691939e0a14be61c93dffe1b8
Donovan Walker <130865355+dreamsalotl@users.noreply.github.com>
2023-07-17 21:12:20 -0500

    fixed cards from displaying incorrectly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ function displaySearchResults(event) {
   var searchTerm = document.getElementById("simple-search").value;
   var oplURL = "https://openlibrary.org/subjects/" + searchTerm.toLowerCase() + ".json";
   var imdbURL = "https://search.imdbot.workers.dev/?q=" + searchTerm + ".json";
+  var previousSearches = [];
+
 
   fetch(oplURL)
     .then(function (response) {
@@ -24,13 +26,10 @@ function displaySearchResults(event) {
 
         var author = data.works[i].authors[0].name;
         var year = data.works[i].first_publish_year;
-        
-        bookResults.appendChild(document.createElement("div"));
-        bookResults.children[i].append(data.works[i][bookTitle]);
-        bookResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
 
         bookElement.setAttribute("onclick", `exploreItem("${author}", "${year}", "${bookTitle}", "${bookCoverURL}")`);
         bookResults.appendChild(bookElement);
+        bookResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
         bookResults.children[i]?.append(bookTitle);
       }
     });
@@ -49,14 +48,11 @@ function displaySearchResults(event) {
         var movieTitle = data.description[i]?.["#TITLE"];
         var movieResults = document.getElementById("movieResults");
         var movieElement = document.createElement("div");
-        
-         movieResults.appendChild(document.createElement("div"));
-         movieResults.children[i].append(data.description[i][movieTitle]);
-         movieResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
 
         console.log("movieTitle", movieTitle);
         movieElement.setAttribute("onclick", `exploreItem("${movieTitle}")`);
         movieResults.appendChild(movieElement);
+        movieResults.children[i].classList.add("style", "box-border", "p-4", "border-4", "border-black", "bg-gray-200", "text-center", "text-2xl", "font-bold", "rounded-lg", "shadow-lg", "hover:bg-gray-300", "hover:shadow-xl", "transition", "duration-500", "ease-in-out", "transform", "hover:-translate-y-1", "hover:scale-110");
         movieResults.children[i]?.append(movieTitle);
       }
     });
